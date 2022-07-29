e628b1219c81cb6a887e6ff7f5aa79e981d030cd
Sol Kim <48229658+Kimsol21@users.noreply.github.com>
2022-07-30 01:44:08 +0900

    [Level1]숫자 문자열과 영단어 (수정)
    string 배열 전역으로 옮기고 함수연산 한번으로 줄여줌.

--- CodingTest_1/CodingTest_1/main.cpp
+++ CodingTest_1/CodingTest_1/main.cpp
@@ -4,14 +4,16 @@
 
 using namespace std;
 
+string NUM[10] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
 int solution(string s) {
     int answer = 0;
-    string NUM[10] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    int findResult = -1;
     for (int i = 0; i < 10; i++)
     {
-        while (s.find(NUM[i]) != string::npos) //함수연산 굳이두번?
+        while (findResult = s.find(NUM[i]) != string::npos)
         {
-            s.replace(s.find(NUM[i]), NUM[i].length(), to_string(i));
+            s.replace(findResult, NUM[i].length(), to_string(i));
         }
     }
     answer = stoi(s);
@@ -28,3 +30,4 @@ int main()
     }   
     return 0;
 }
+
