9f60e18c567031f411c13979c39f4a8c7b4c76d1
Sol Kim <48229658+Kimsol21@users.noreply.github.com>
2022-08-02 23:54:54 +0900

    [Level 1] 체육복
    https://school.programmers.co.kr/learn/courses/30/lessons/42862

--- CodingTest_1/CodingTest_1/main.cpp
+++ CodingTest_1/CodingTest_1/main.cpp
@@ -1,28 +1,78 @@
 #include <iostream>
-#include <cmath>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-long long solution(int w, int h) {
-    long long answer = 0;
-    double begin = 0, finish;
-    for (int i = 1; i <= w; i++)
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    vector<pair<int, int>> Students;
+    Students.reserve(n);
+    for (int i = 0; i < n; ++i)
     {
-        finish = (double)i * h / w;
-        answer += ceil(finish) - floor(begin);
-        begin = finish;
+        Students.push_back(make_pair(i, 1));
     }
-    return (long long)w * h - answer;
+    for (unsigned int i = 0; i < lost.size(); i++)
+    {
+        Students[lost[i]-1].second -= 1;
+    }
+    for (unsigned int i = 0; i < reserve.size(); i++)
+    {
+        Students[reserve[i]-1].second += 1;
+    }
+
+    vector<pair<int, int>>::iterator iter;
+    for (iter = Students.begin(); iter != Students.end(); ++iter)
+    {
+        int clothes = iter->second;
+        if (iter == Students.begin()) 
+        {
+            if (clothes == 2 && (iter+1)->second < 1)
+            {
+                (iter+1)->second += 1;
+                continue;
+            }
+            else continue;
+        }
+        else if (iter == Students.end() - 1)
+        {
+            if (clothes == 2 && (iter - 1)->second < 1)
+            {
+                (iter - 1)->second += 1;
+                continue;
+            }
+            else continue;
+        }
+        else
+        {
+            if (clothes == 2 && (iter - 1)->second < 1)
+            {
+                (iter - 1)->second += 1;
+                continue;
+            }
+            else if (clothes == 2 && (iter + 1)->second < 1)
+            {
+                (iter + 1)->second += 1;
+                continue;
+            }
+        }       
+    }
+    for (iter = Students.begin(); iter != Students.end(); ++iter)
+    {
+        if (iter->second >= 1)
+            answer += 1;
+    }
+
+    return answer;
 }
 
 int main()
 {
-    while (1)
-    {
-        int w, h;
-        cin >> w>>h;
-        cout << solution(w, h) << endl;
-    }   
+    vector<int> lost = { 2,4 };
+    vector<int> reserve = { 1, 3, 5 };
+    int n = 5;
+
+    cout << solution(n, lost, reserve);
 
     return 0;
 }
