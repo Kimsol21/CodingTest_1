2aa236495c7b6908052ad64bc8d306323adade71
Sol Kim <48229658+Kimsol21@users.noreply.github.com>
2022-08-06 02:00:05 +0900

    [Level 1] 2016년
    https://school.programmers.co.kr/learn/courses/30/lessons/12901

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -1,44 +1,22 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-string solution(vector<string> participant, vector<string> completion) {
+string solution(int a, int b) {
     string answer = "";
-    vector <pair<string, int>> totalAthlete;
-    sort(participant.begin(), participant.end());
-    int index = -1;
-    for (int i = 0; i < participant.size(); i++)
-    {
-        if (i > 0 && participant[i] == participant[i - 1]) totalAthlete[index].second++;
-        else
-        {
-            totalAthlete.push_back(make_pair(participant[i], 1));
-            index++;
-        }
-    }
-    for (int i = 0; i < completion.size(); i++)
-    {
-        for (int k = 0; k < totalAthlete.size(); k++)
-        {
-            if (totalAthlete[k].first == completion[i])
-            {
-                totalAthlete[k].second -= 1;
-                break;
-            }
-        }
-    }
-    for (int i = 0; i < totalAthlete.size(); i++) if (totalAthlete[i].second == 1) return totalAthlete[i].first;
+    vector<string> week = {  "THU","FRI", "SAT" ,"SUN", "MON", "TUE", "WED"};
+    vector<int> month = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int days = 0;
+    for (int i = 1; i < a; i++) days += month[i-1];
+    days += b;
+    answer = week[days % 7];
     return answer;
 }
 
 int main()
 {
-    vector<string> participant = { "mislav", "stanko", "mislav", "ana" };
-    vector<string> completion = { "stanko", "ana", "mislav" };
-    cout << solution(participant, completion) << endl;
-    return 0;
+    cout << solution(5, 24) << endl;
 }
 
