2f5579abc59ee78ab043d91cf6717536ac11b253
Sol Kim <48229658+Kimsol21@users.noreply.github.com>
2022-08-07 20:42:39 +0900

    [Level 1] 모의고사
    https://school.programmers.co.kr/learn/courses/30/lessons/42840

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-string solution(int a, int b) {
-    string answer = "";
-    vector<string> week = {  "THU","FRI", "SAT" ,"SUN", "MON", "TUE", "WED"};
-    vector<int> month = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    int days = 0;
-    for (int i = 1; i < a; i++) days += month[i-1];
-    days += b;
-    answer = week[days % 7];
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    int order1[5] = { 1, 2, 3, 4, 5 };
+    int order2[8] = { 2, 1, 2, 3, 2, 4, 2, 5};
+    int order3[10] = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+    vector<int> scores = { 0, 0, 0 };
+    int highScore = 0;
+    for (int i = 0; i<answers.size(); i++)
+    {
+        if (answers[i] == order1[i % 5]) scores[0]++;
+        if (answers[i] == order2[i % 8]) scores[1]++;
+        if (answers[i] == order3[i % 10]) scores[2]++;
+    }
+    highScore = *max_element(scores.begin(), scores.end());
+    for (int i = 0; i < scores.size(); i++) 
+    {
+        if (scores[i] == highScore) answer.push_back(i+1);
+    }  
     return answer;
 }
 
 int main()
 {
-    cout << solution(5, 24) << endl;
+    vector<int>answers = { 1, 2, 3, 4, 5 };
+    vector<int>output = solution(answers);
+    for (int i = 0; i < output.size(); i++) cout << output[i] << endl;
 }
 
