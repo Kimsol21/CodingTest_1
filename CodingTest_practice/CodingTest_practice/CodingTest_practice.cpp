1dd3ede6ccfecebe3921e391a312fe35367c500e
Sol Kim <48229658+Kimsol21@users.noreply.github.com>
2022-08-09 19:43:04 +0900

    [Level 1] 두 정수 사이의 합
    https://school.programmers.co.kr/learn/courses/30/lessons/12912

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <set>
 
 using namespace std;
 
-vector<int> solution(vector<int> numbers) {
-    vector<int> answer;
-    set<int> result;
-    int num = numbers.size();
-    for (int i = 0; i < num; i++)
-    {
-        for(int k = i + 1; k < num; k++)
-        {
-            result.insert(numbers[i] + numbers[k]);
-        }
-    }
-    for (int i : result)
-    {
-        answer.push_back(i);
-    }
+long long solution(int a, int b) {
+    long long answer = 0;
+    if (a <= b) for (int i = a; i <= b; i++) answer += i;
+    else for (int i = b; i <= a; i++) answer += i;
     return answer;
 }
 
 int main()
 {
-    vector<int>numbers = { 2, 1, 3, 4, 1 };
-    vector<int>output = solution(numbers);
-    for (int i = 0; i < output.size(); i++) cout << output[i] << endl;
+    cout << solution(5, 3) << endl;
 }
 
