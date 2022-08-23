1c92dd861da840b8acd6e590567a1cc6a1eec9b1
Sol Kim <48229658+Kimsol21@users.noreply.github.com>
2022-08-23 22:43:07 +0900

    [Level 1] 예산
    https://school.programmers.co.kr/learn/courses/30/lessons/12982

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<int> arr)
-{
-    vector<int> answer;
-    answer.push_back(arr[0]);
-    for (int i = 1; i < arr.size(); i++)
+int solution(const vector<int> d, int budget) {
+    int answer = 0;
+    vector<int> temp = d;
+    sort(temp.begin(), temp.end());
+    for (auto i : temp)
     {
-        if (arr[i - 1] == arr[i]) continue;
-        else answer.push_back(arr[i]);
+        budget -= i;
+        if (budget < 0) break;
+        else answer++;
     }
     return answer;
 }
 
 int main()
 {
-    vector<int>numbers = { 1,1,3,3,0,1,1 };
-    for (auto i : solution(numbers))
-        cout << i << endl;
+    cout << solution({ 1,3,2,5,4 }, 9) << endl;
+    return 0;
 }
 
