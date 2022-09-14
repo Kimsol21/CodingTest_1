6d45cfc399371650b2678ceed0abc82d4fc96fa1
Sol Kim <48229658+Kimsol21@users.noreply.github.com>
2022-09-14 22:21:03 +0900

    [Level 1] 최대공약수와 최소공배수
    https://school.programmers.co.kr/learn/courses/30/lessons/12940

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-string solution(string phone_number) {
-    string answer = "";
-    int length = phone_number.size();
-    for (int i = 0; i < length; i++)
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    vector<int> nDivisor;//약수
+    vector<int> mDivisor;
+    vector<int> CommonDivisor; //공약수
+    int greatestCommonDivisor = 0; //최대공약수
+    int leastCommonMultiple = 0; //최소공배수
+
+    for (int i = 1; i < n+1; i++)
     {
-        if (length - i < 5)
-        {
-            answer += phone_number[i];
-            continue;
-        }
-        else
-        {
-            answer += '*';
-        }
+        if (n % i == 0) nDivisor.push_back(i);
     }
+    for (int i = 1; i < m+1; i++)
+    {
+        if (m % i == 0) mDivisor.push_back(i);
+    }
+    for (auto i : nDivisor)
+    {
+        if (find(mDivisor.begin(), mDivisor.end(), i) == mDivisor.end()) continue;
+        else greatestCommonDivisor = i;
+    }
+    leastCommonMultiple = n / greatestCommonDivisor * m;
+    answer.push_back(greatestCommonDivisor);
+    answer.push_back(leastCommonMultiple);
+
     return answer;
 }
 
-int main()
-{
-    cout << solution("01033334444") << endl; //return "*******4444"
+int main(void) {
+    vector<int> answer = solution(3, 12);
+    for (auto i : answer)
+        cout << i << endl;
     return 0;
 }
+
 
