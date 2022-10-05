2dc35ea6c57a914aaa6fb104bd60cc062db34966
KimSol <ksol92789@gmail.com>
2022-10-05 20:10:05 +0900

    [LeetCode] Trapping Rain Water
    https://leetcode.com/problems/trapping-rain-water/

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -1,43 +1,37 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> solution(int n, int m) {
-    vector<int> answer;
-    vector<int> nDivisor;//약수
-    vector<int> mDivisor;
-    vector<int> CommonDivisor; //공약수
-    int greatestCommonDivisor = 0; //최대공약수
-    int leastCommonMultiple = 0; //최소공배수
-
-    for (int i = 1; i < n+1; i++)
-    {
-        if (n % i == 0) nDivisor.push_back(i);
-    }
-    for (int i = 1; i < m+1; i++)
-    {
-        if (m % i == 0) mDivisor.push_back(i);
-    }
-    for (auto i : nDivisor)
-    {
-        if (find(mDivisor.begin(), mDivisor.end(), i) == mDivisor.end()) continue;
-        else greatestCommonDivisor = i;
-    }
-    leastCommonMultiple = n / greatestCommonDivisor * m;
-    answer.push_back(greatestCommonDivisor);
-    answer.push_back(leastCommonMultiple);
-
-    return answer;
+ int solution(vector<int>& height) {
+     int answer = 0;
+     int maxHeightIndex = max_element(height.begin(), height.end())-height.begin();
+     int maxHeight = 0;
+     for (int i = 0; i < maxHeightIndex; i++)
+     {
+         if (maxHeight <= height[i]) maxHeight = height[i];
+         else
+         {
+             answer += maxHeight - height[i];
+         }
+     }
+     maxHeight = 0;
+     for (int i = height.size() - 1; i > maxHeightIndex; i--)
+     {
+         if (maxHeight <= height[i]) maxHeight = height[i];
+         else
+         {
+             answer += maxHeight - height[i];
+         }
+     }
+     return answer;
 }
 
-int main(void) {
-    vector<int> answer = solution(3, 12);
-    for (auto i : answer)
-        cout << i << endl;
-    return 0;
+ int main(void) {
+     vector<int> answer = {4,2,0,3,2,5 };
+     cout << solution(answer) << endl;
+     return 0;
 }
 
 
