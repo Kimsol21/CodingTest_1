c3e564f0c033f69c11e88387787c3ea39dad2300
KimSol <ksol92789@gmail.com>
2022-10-19 17:08:17 +0900

    [Level 1] 카카오 비밀지도
    https://school.programmers.co.kr/learn/courses/30/lessons/17681

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -1,29 +1,67 @@
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
-#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-int solution(int n) {
-    int answer = 0;
-    int num = n;
-    vector<int>Nums;
-    while (num >= 3)
+vector<int> ChangeNum(int n, int max)
+{
+    vector<int> retval;
+    while (n >= 2)
     {
-        Nums.push_back(num % 3);
-        num = num / 3;
+        retval.push_back(n % 2);
+        n /= 2;    
     }
-    Nums.push_back(num);
-    for (int i = 0; i < Nums.size(); i++)
+    if (n > 0)
     {
-        answer += Nums[i] * pow(3, Nums.size() - i-1);
+        retval.push_back(1);
+    }    
+    while (retval.size() < max)
+    {
+        retval.push_back(0);
+    }
+    reverse(retval.begin(), retval.end());
+    return retval;
+}
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    string tempS;
+    vector<vector<int>> map1;
+    vector<vector<int>> map2;
+    for (int i = 0; i < n; i++)
+    {
+        map1.push_back(ChangeNum(arr1[i], n));
+        map2.push_back(ChangeNum(arr2[i], n));
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (map1[i][j] == 1 || map2[i][j] == 1)
+            {
+                tempS += '#';
+                continue;
+            }
+            else
+            {
+                tempS += ' ';
+                continue;
+            }
+        }
+        answer.push_back(tempS);
+        tempS = "";
     }
     return answer;
 }
 
  int main(void) {
-     cout << solution(45) << endl; //return 7
+     vector<string> answer = solution(5, { 0,20,28,18,11 }, { 0,1,21,17,28 });
+     for (string s : answer)
+     {
+         cout << s << endl;
+     }
      return 0;
 }
 
