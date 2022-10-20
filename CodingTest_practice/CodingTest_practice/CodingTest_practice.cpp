ee0a129607b358b84c70f4851014e600a5d30c9c
KimSol <ksol92789@gmail.com>
2022-10-20 18:15:32 +0900

    [Level 1] 문자열 내 마음대로 정렬하기
    https://school.programmers.co.kr/learn/courses/30/lessons/12915

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -5,60 +5,24 @@
 
 using namespace std;
 
-vector<int> ChangeNum(int n, int max)
-{
-    vector<int> retval;
-    while (n >= 2)
-    {
-        retval.push_back(n % 2);
-        n /= 2;    
-    }
-    if (n > 0)
-    {
-        retval.push_back(1);
-    }    
-    while (retval.size() < max)
-    {
-        retval.push_back(0);
-    }
-    reverse(retval.begin(), retval.end());
-    return retval;
-}
-
-vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+vector<string> solution(vector<string> strings, int n) {
     vector<string> answer;
-    string tempS;
-    vector<vector<int>> map1;
-    vector<vector<int>> map2;
-    for (int i = 0; i < n; i++)
+    vector<pair<char, string>> temp;
+    for (string s : strings)
     {
-        map1.push_back(ChangeNum(arr1[i], n));
-        map2.push_back(ChangeNum(arr2[i], n));
+        temp.push_back(make_pair(s[n], s));
     }
-    for (int i = 0; i < n; i++)
+    sort(temp.begin(), temp.end());
+    for (pair<char, string> p : temp)
     {
-        for (int j = 0; j < n; j++)
-        {
-            if (map1[i][j] == 1 || map2[i][j] == 1)
-            {
-                tempS += '#';
-                continue;
-            }
-            else
-            {
-                tempS += ' ';
-                continue;
-            }
-        }
-        answer.push_back(tempS);
-        tempS = "";
+        answer.push_back(p.second);
     }
     return answer;
 }
 
  int main(void) {
-     vector<string> answer = solution(5, { 0,20,28,18,11 }, { 0,1,21,17,28 });
-     for (string s : answer)
+     vector<string> result = solution({ "abce" ,"abcd","cdx" }, 1);
+     for (string s : result)
      {
          cout << s << endl;
      }
