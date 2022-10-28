f107f2b0fd95cf3b14ea91fc6342cd372146b2d6
KimSol <ksol92789@gmail.com>
2022-10-28 19:55:33 +0900

    [Level 2] 최댓값과 최솟값
    https://school.programmers.co.kr/learn/courses/30/lessons/12939

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -5,27 +5,39 @@
 
 using namespace std;
 
-vector<string> solution(vector<string> strings, int n) {
-    vector<string> answer;
-    vector<pair<char, string>> temp;
-    for (string s : strings)
+string solution(string s) {
+    string answer = "";
+    string temp = "";
+    vector<int> Numbers;
+    int max = 0, min = 0;
+    for (char c : s)
     {
-        temp.push_back(make_pair(s[n], s));
+        if (' ' == c)
+        {
+            Numbers.push_back(stoi(temp));
+            temp = "";
+        }
+        else
+        {
+            temp += c;
+        }
     }
-    sort(temp.begin(), temp.end());
-    for (pair<char, string> p : temp)
+    if (temp != "")
     {
-        answer.push_back(p.second);
+        Numbers.push_back(stoi(temp));
     }
+    sort(Numbers.begin(), Numbers.end());
+
+    answer += to_string(Numbers[0]);
+    answer += " ";
+    answer += to_string(Numbers[Numbers.size() - 1]);
+
     return answer;
 }
 
  int main(void) {
-     vector<string> result = solution({ "abce" ,"abcd","cdx" }, 1);
-     for (string s : result)
-     {
-         cout << s << endl;
-     }
+     string result = solution({ "-1 -2 -3 -4"	 });
+     cout << result << endl;
      return 0;
 }
 
