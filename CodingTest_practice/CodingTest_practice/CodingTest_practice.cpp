14938401eeb4228c89eac363c5d126622ccebfeb
KimSol <ksol92789@gmail.com>
2022-11-07 21:52:36 +0900

    [Level 2] 최솟값 만들기
    https://school.programmers.co.kr/learn/courses/30/lessons/12941

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-string solution(string s) {
-    string answer = "";
-    
-    answer += toupper(s[0]);
-    for (int i = 1; i < s.size(); i++)
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    for (int i = 0; i < A.size(); i++)
     {
-        if (s[i - 1] == ' ')
-        {
-            answer += toupper(s[i]);
-        }
-        else
-        {
-            answer += tolower(s[i]);
-        }
+        answer += A[i] * B[A.size() - i - 1];
     }
+
     return answer;
 }
 
  int main(void) {
-     string result = solution({ "  3peoPle unFollowed me  hap "	 });//return 3people Unfollowed Me
-     cout << result << endl;
+     vector<int> A = {1,4,2}; //answer = 29
+     vector<int> B = {5,4,4};
+
+     cout << solution(A, B)<< endl;
      return 0;
 }
 
