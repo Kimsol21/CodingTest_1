9b29a3c26c58cad07d2bad93153e5aad54be0c72
KimSol <ksol92789@gmail.com>
2022-11-23 19:35:44 +0900

    [Level 2] 피보나치 수
    https://school.programmers.co.kr/learn/courses/30/lessons/12945

--- CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
+++ CodingTest_practice/CodingTest_practice/CodingTest_practice.cpp
@@ -4,27 +4,35 @@
 
 using namespace std;
 
-int solution(int n) {
-    int answer = 0;
-    int sumTotal = 0;
-
-    for (int startNum = 1; startNum < n / 2+1; startNum++)
+int solution(int n) { //nÀº 2,3,4...
+    long long answer = 0;
+    long long temp = 0;
+    int count = 2;
+    long long fiv1 = 0;
+    long long fiv2 = 1;
+    
+    while (count <= n)
     {
-        int plus = 0;
-        while (sumTotal < n)
+        if (1234567 <= answer)
         {
-            sumTotal += startNum + plus;
-            plus++;
+            answer %= 1234567;
+            fiv1 %= 1234567;
+            fiv2 %= 1234567;
         }
-        if (sumTotal == n) answer++;
-        sumTotal = 0;
+        temp += fiv1 + fiv2;
+        fiv1 = fiv2;
+        fiv2 = temp;
+        answer = temp;
+        temp = 0;
+        count++;
     }
-    return answer+1;
+
+    return answer % 1234567;
 }
 
 int main()
 {
-    cout << solution(15) << endl; //result 4
+    cout << solution(47) << endl; //result 5
     return 0;
 }
 
